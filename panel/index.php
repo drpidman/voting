a0bbbce45e0f3a6c2358249be96c84dc81b09bd8
drpidman <augustocaio663@gmail.com>
2023-09-06 11:55:22 -0300

    Preparação para o painel de controle

---
+++ panel/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../css/components/navbar.css">
+    <link rel="stylesheet" href="../css/components/common/all.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <section class="nav-item">
+            <h1>ESantos Soluções</h1>
+        </section>
+        <section class="nav-item">
+
+        </section>
+        <section class="nav-item">
+
+        </section>
+    </nav>
+</body>
+
+</html>
