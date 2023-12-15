63bb8ca4d4f4dd6f4f07d370bb3a2e2f96ea3cc7
drpidman <augustocaio663@gmail.com>
2023-12-15 03:29:17 -0300

    Logo da empresa

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
-    <section class="nav-item">
-        <h1>---</h1>
+    <section class="nav-item nav-image">
+        <img src="<?php echo URL_SUBFOLDER ?>/public/source_images/esantostech_white.png" alt="canditate">
     </section>
     <section class="nav-item">
 
